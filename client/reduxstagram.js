ac32754bbe19ca8ae924a62f007128d7216a2c62
Crezell Nortje <cnortje@hotmail.com>
2019-04-29 12:53:34 +0200

    amend last commit

--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -4,4 +4,21 @@ import { render } from "react-dom";
 // import css
 import css from "./styles/style.styl";
 
-render(<p>hi</p>, document.getElementById("root"));
+// import components
+import Main from "./components/Main";
+import Single from "./components/Single";
+import PhotoGrid from "./components/PhotoGrid";
+
+// import react router
+import { Router, Route, IndexRoute, browserHistory } from "react-router";
+
+const router = (
+  <Router history={browserHistory}>
+    <Route path="/" component={Main}>
+      <IndexRoute component={PhotoGrid} />
+      <Route path="/view/:postId" component={Single} />
+    </Route>
+  </Router>
+);
+
+render(router, document.getElementById("root"));
