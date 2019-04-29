84dc945d7c2eb0eb2ad8474b96008fbb8a44312e
Crezell Nortje <cnortje@hotmail.com>
2019-04-29 14:29:12 +0200

    created photo component

--- client/components/PhotoGrid.js
+++ client/components/PhotoGrid.js
@@ -1,7 +1,14 @@
 import React from "react";
+import Photo from "./Photo";
 
 export default class PhotoGrid extends React.Component {
   render() {
-    return <div className="photo-grid">test</div>;
+    return (
+      <div className="photo-grid">
+        {this.props.posts.map((post, index) => (
+          <Photo {...this.props} key={index} index={index} post={post} />
+        ))}
+      </div>
+    );
   }
 }
