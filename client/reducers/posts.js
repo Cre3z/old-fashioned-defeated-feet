0e28e6da61d7c309dec53bbb16fe6fe719f69ef4
Crezell Nortje <cnortje@hotmail.com>
2019-04-29 13:28:14 +0200

    created reducers

---
+++ client/reducers/posts.js
@@ -0,0 +1,10 @@
+//  a reducer takes in two things
+
+// 1. the action (info about what happened)
+// 2. copy of current state
+// never mutate the store but always copy and return a new copy of the store
+
+function posts(state = [], action) {
+  return state;
+}
+export default posts;
